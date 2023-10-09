79bec433bddc8fd0bf3c9fcfbf722d717486208c
Ayush-Dabas <aydbs23@gmail.com>
2023-10-09 21:40:15 +0530

    initialized file

---
+++ array_3.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void kthElement(vector<int> a, int k)
+{
+}
+
+int main()
+{
+    vector<int> a = {-12, 23, 6, 2, 7, 2, 7564, 0};
+    int k = 2;
+    kthElement(a, k);
+    return 0;
+}
