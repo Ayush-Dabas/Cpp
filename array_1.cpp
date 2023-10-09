7fc52d65e19afc77eed1bccb1a2d30900384ffb7
Ayush-Dabas <aydbs23@gmail.com>
2023-10-09 20:41:34 +0530

    question completed

--- array_1.cpp
+++ array_1.cpp
@@ -1,8 +1,30 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// Reverse the array
+void reverse(vector<int> &a)
+{
+    int n = a.size();
+    for (int i = 0; i < n / 2; i++)
+    {
+        swap(a[i], a[n - 1 - i]);
+    }
+}
+
+void print(vector<int> a)
+{
+    for (int i = 0; i < a.size(); i++)
+    {
+        cout << a[i] << " ";
+    }
+    cout << "\n";
+}
+
 int main()
 {
-    cout << "Hello world !";
+    vector<int> a = {1, 2, 3, 4, 5, 6};
+    reverse(a);
+    print(a);
+
     return 0;
 }
