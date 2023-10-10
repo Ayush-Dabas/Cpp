726e6e8b6c159e0ad9ab2284783693a867c20716
Ayush-Dabas <aydbs23@gmail.com>
2023-10-10 05:33:39 +0530

    kth done

--- array_3.cpp
+++ array_3.cpp
@@ -3,12 +3,15 @@ using namespace std;
 
 void kthElement(vector<int> a, int k)
 {
+    sort(a.begin(), a.end());
+    cout << "Minimum " << k << " th element: " << a[k - 1] << "\n";
+    cout << "Maximum " << k << " th element: " << a[a.size() - k];
 }
 
 int main()
 {
     vector<int> a = {-12, 23, 6, 2, 7, 2, 7564, 0};
-    int k = 2;
+    int k = 4;
     kthElement(a, k);
     return 0;
 }
